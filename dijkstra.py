9c4585cabb355eecc24bd282fae2f9b97a5b97f7
MartinoPamela <s284756@studenti.polito.it>
2024-07-03 19:59:47 +0200

    versione aggiornata

--- dijkstra.py
+++ dijkstra.py
@@ -6,6 +6,10 @@ import random
 G = nx.directed_havel_hakimi_graph([3] * 15,
                                    [3] * 15,
                                    create_using=None)
+# è un grafo che è già fornito dalla libreria, havel_hakimi è una particolare struttura a grafo
+# in cui posso assegnare la dimensionalità dei nodi, quindi gli passo come parametro due liste,
+# in particolare gli sto dicendo che ha 15 nodi che hanno in degree = 3 quindi 3 archi entranti
+# e hanno tre archi uscenti, e poi gli assegna dei pesi randomici
 
 for e in G.edges():
     G[e[0]][e[1]]['weight'] = random.randrange(1,10)
@@ -15,8 +19,9 @@ print(G.nodes())
 print(G.edges())
 
 #getting shortest path
-print(nx.dijkstra_path(G, 0, 8))
-print(nx.dijkstra_path_length(G, 0, 7))
+print(nx.dijkstra_path(G, 0, 8))  # mi dà direttamente il percorso, mi dà una lista di nodi
+print(nx.dijkstra_path_length(G, 0, 7))  # implementazione che mi dà la lunghezza,
+# qui gli sto già assegnando anche un target
 
 optpath = nx.dijkstra_path(G, 0, 7)
 optedges = []
